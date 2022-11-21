2ba47a7c26314316699506e33d01406143cfbe40
Yogesh Mane <yogeshmane008@gmail.com>
2022-11-21 18:22:57 +0530

    21-11 Third Commi

--- src/test/java/Practice/AutoSuggestion.java
+++ src/test/java/Practice/AutoSuggestion.java
@@ -2,12 +2,14 @@ package Practice;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
@@ -35,6 +37,7 @@ public class AutoSuggestion {
 		rm=new ChromeDriver(co);
 		
 		rm.get("https://www.google.com/");
+	
 				
 	}
 	
@@ -47,21 +50,50 @@ public class AutoSuggestion {
 		
 		System.out.println(rm.getTitle());
 		
-		rm.findElement(By.xpath("//*[contains(@class,'gLFyf')]")).sendKeys("Disha");
+		rm.findElement(By.xpath("//*[contains(@class,'gLFyf')]")).sendKeys("Dubai");
 		
 		Thread.sleep(4000);
 		
-		TakesScreenshot tk=(TakesScreenshot)rm;
+		List<WebElement> le= rm.findElements(By.xpath("//*[contains(@class,'sbct')]"));
 		
-		File src=tk.getScreenshotAs(OutputType.FILE);
 	
-		try {
-			FileUtils.copyFile(src, new File("C://Users//Admin//eclipse-workspace//NewPractice//Screenshot//GoogleAutosuggestionList.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		int TotalNumber=le.size();
+		
+		System.out.println("Total number of Autosuggestions are :" +TotalNumber);
+
+		Thread.sleep(3000);
+		
+		for(WebElement ele:le)
+		{
+			
+			String Text= ele.getText();
+			
+			System.out.println(Text);
+			
+			if(Text.equalsIgnoreCase("dubai time right now"))
+			{
+				Thread.sleep(3000);
+				
+				TakesScreenshot tk=(TakesScreenshot)rm;
+				
+				File src=tk.getScreenshotAs(OutputType.FILE);
+			
+				try {
+					FileUtils.copyFile(src, new File("C://Users//Admin//eclipse-workspace//NewPractice//Screenshot//GoogleAutosuggestionList.png"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+				
+				ele.click();
+				
+				break;
+			}
+			
 		}
-	}
+		
+		}
 	
 	
 	@AfterMethod
