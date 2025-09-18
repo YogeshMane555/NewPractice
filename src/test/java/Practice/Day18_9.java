605456a1b81edcd8998ccfb102fc2277de092c6a
Yogesh <Yogesh@192.168.1.38>
2025-09-18 15:47:10 +0530

    Commit 18_9

--- src/test/java/Practice/Day18_9.java
+++ src/test/java/Practice/Day18_9.java
@@ -21,36 +21,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-
-
-public class Day16_9 {
+public class Day18_9 {
 
 	public WebDriver km;
-	
-	public static ExtentReports report;
-	
-	public ExtentTest test;
-	
-	public ExtentReports extent;
 	
 	@BeforeMethod
 	public void setUp()
 	{
-		ExtentSparkReporter htmlReporter=new ExtentSparkReporter("ExtentReport.html");
-		
-		extent=new ExtentReports();
-		
-		extent.attachReporter(htmlReporter);
-		
-		test=extent.createTest("RahulShetty","To Test all fucntionality on webpage");
-		
-		
-		
-		System.setProperty("wedriver.chrome.driver", "C:\\Users\\Yogesh\\git\\NewPractice\\ChromeDriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Yogesh\\git\\NewPractice\\ChromeDriver\\chromedriver.exe");
 		
 		km=new ChromeDriver();
 		
@@ -59,36 +37,31 @@ public class Day16_9 {
 		km.manage().deleteAllCookies();
 		
 		km.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
-		
-		test.log(Status.INFO, "Starting Test Case");
-		
-		test.pass("Navigated to Rahul Shetty Practice WebSite");
 	}
 	
 	@Test
 	public void tc1()
 	{
-		List<WebElement> tags=km.findElements(By.tagName("a"));
+		List<WebElement> tag=km.findElements(By.tagName("a"));
 		
-		int noTags=tags.size();
+		int noTag=tag.size();
 		
-		System.out.println("Total Number of Tags :"+noTags);
+		System.out.println("Total Number of Tags :"+noTag);
 		
-		for(WebElement tagst:tags)
+		for(WebElement tagt:tag)
 		{
-			String tagtext=tagst.getText();
-		
-			System.out.println("Tags are :"+tagtext);
+			String tagText=tagt.getText();
+			
+			System.out.println("Tags are :"+tagText);
 		}
 		
 		String title=km.getTitle();
 		
-		System.out.println("Title of Current WebPage is :"+title);
+		System.out.println("Title of Current Title is :"+title);
 		
 		String url=km.getCurrentUrl();
 		
-		System.out.println("URL of Current WebPage is :"+url);
+		System.out.println("URL of Current URL is :"+url);
 	}
 	
 	@Test
@@ -100,14 +73,14 @@ public class Day16_9 {
 		
 		boolean b2=r3.isSelected();
 		
-		System.out.println("Is Radio button 3 Selected ?:"+b2);
+		System.out.println("Radio Button 3 Selected ?:"+b2);
 		
 		TakesScreenshot tk2=(TakesScreenshot)km;
 		
 		File src2=tk2.getScreenshotAs(OutputType.FILE);
 		
 		try {
-			FileUtils.copyFile(src2, new File("C:\\Users\\Yogesh\\git\\NewPractice\\Screenshot//16_9RADIO.png"));
+			FileUtils.copyFile(src2, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9RADIOAButton.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -117,40 +90,43 @@ public class Day16_9 {
 	@Test
 	public void tc3() throws InterruptedException
 	{
-		km.findElement(By.xpath("//input[contains(@id,'autocomplete')]")).sendKeys("Se");
+		km.findElement(By.xpath("//input[contains(@id,'autocomplete')]")).sendKeys("Sh");
 		
 		Thread.sleep(2000);
 		
 		List<WebElement> auto=km.findElements(By.xpath("//li[contains(@class,'ui-menu-item')]"));
 		
-		int autoSize=auto.size();
+		int autoNo=auto.size();
 		
-		System.out.println("Total Number of Autosuggestion are :"+autoSize);
+		System.out.println("Total Number of Autosuggestions are :"+autoNo);
 		
 		for(WebElement autot:auto)
 		{
 			String autotxt=autot.getText();
 			
-			System.out.println("Autosuggestion list are :"+autotxt);
+			System.out.println("Autosuggest Lists are :"+autotxt);
 			
-			if(autotxt.contentEquals("Seychelles"))
+			if(autotxt.contentEquals("Virgin Islands (British)"))
 			{
 				autot.click();
+				
+				Thread.sleep(2000);
+				
+				TakesScreenshot tk3=(TakesScreenshot)km;
+				
+				File src3=tk3.getScreenshotAs(OutputType.FILE);
+				
+				try {
+					FileUtils.copyFile(src3, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9AUTOSUGGESTION.png"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			
-			TakesScreenshot tk3=(TakesScreenshot)km;
-			
-			File src3=tk3.getScreenshotAs(OutputType.FILE);
-			
-			try {
-				FileUtils.copyFile(src3, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9AUTOSUGGESTION.png"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 	}
-	
+
 	@Test
 	public void tc4()
 	{
@@ -165,7 +141,7 @@ public class Day16_9 {
 		File src4=tk4.getScreenshotAs(OutputType.FILE);
 		
 		try {
-			FileUtils.copyFile(src4, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9DROPDOWN.png"));
+			FileUtils.copyFile(src4, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9DROPDOWN.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -179,31 +155,30 @@ public class Day16_9 {
 		
 		c3.click();
 		
-		boolean b5=c3.isSelected();
+		boolean b3=c3.isSelected();
 		
-		System.out.println("Checkbox 3 Selected ?:"+b5);
+		System.out.println("Checkbox 3 Selected ?:"+b3);
 		
 		TakesScreenshot tk5=(TakesScreenshot)km;
 		
 		File src5=tk5.getScreenshotAs(OutputType.FILE);
 		
 		try {
-			FileUtils.copyFile(src5, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9CHECKBOX.png"));
+			FileUtils.copyFile(src5, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9CHECKBOX.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	
 	@Test
 	public void tc6() throws InterruptedException
 	{
 		km.findElement(By.xpath("//a[contains(@id,'opentab')]")).click();
 		
-		Set<String>s= km.getWindowHandles();
+		Set<String> s=km.getWindowHandles();
 		
-		Iterator<String>ss=s.iterator();
+		Iterator<String>ss= s.iterator();
 		
 		String parent=ss.next();
 		
@@ -224,7 +199,7 @@ public class Day16_9 {
 		File src6=tk6.getScreenshotAs(OutputType.FILE);
 		
 		try {
-			FileUtils.copyFile(src6, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9NEWTAB.png"));
+			FileUtils.copyFile(src6, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9NEWTAB.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -234,11 +209,9 @@ public class Day16_9 {
 	@Test
 	public void tc7() throws InterruptedException
 	{
-		WebElement text=km.findElement(By.xpath("//input[contains(@name,'enter-name')]"));
+		WebElement alertTxt=km.findElement(By.xpath("//input[contains(@id,'name')]"));
 		
-		Thread.sleep(2000);
-		
-		text.sendKeys("YOGI");
+		alertTxt.sendKeys("YOGESHHHH");
 		
 		Thread.sleep(2000);
 		
@@ -252,7 +225,11 @@ public class Day16_9 {
 		
 		a.accept();
 		
-		text.sendKeys("MANE");
+		Thread.sleep(2000);
+		
+		alertTxt.sendKeys("MANEEEE");
+		
+		Thread.sleep(2000);
 		
 		km.findElement(By.xpath("//input[contains(@id,'confirmbtn')]")).click();
 		
@@ -263,7 +240,7 @@ public class Day16_9 {
 		Thread.sleep(2000);
 		
 		aa.dismiss();
-		
+	
 	}
 	
 	@Test
@@ -275,43 +252,41 @@ public class Day16_9 {
 		
 		Thread.sleep(2000);
 		
-		WebElement textDisp=km.findElement(By.xpath("//input[contains(@id,'displayed-text')]"));
+		WebElement dispText=km.findElement(By.xpath("//input[contains(@id,'displayed-text')]"));
 		
-		textDisp.sendKeys("YOGESHWAR");
-		
-		Thread.sleep(2000);
+		dispText.sendKeys("RAHUL SHETTY");
 		
 		km.findElement(By.xpath("//input[contains(@id,'hide-textbox')]")).click();
 		
-		boolean b8=textDisp.isDisplayed();
+		boolean b8=dispText.isDisplayed();
 		
-		System.out.println("Text Dsplayed after clicking on Hide button ?:"+b8);
-		
-		Thread.sleep(2000);
+		System.out.println("After Clicking on Hide button TextBox Displayed ?:"+b8);
 		
 		TakesScreenshot tk8=(TakesScreenshot)km;
 		
 		File src8=tk8.getScreenshotAs(OutputType.FILE);
 		
 		try {
-			FileUtils.copyFile(src8, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9AFTERCLICKINGONHIDEBUTTON.png"));
+			FileUtils.copyFile(src8, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9AfterClickingHIDEButton.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		Thread.sleep(2000);
+		
 		km.findElement(By.xpath("//input[contains(@id,'show-textbox')]")).click();
 		
-		boolean b9=textDisp.isDisplayed();
+		boolean b9=dispText.isDisplayed();
 		
-		System.out.println("Text Displayed after clicking on Show Button ?:"+b9);
+		System.out.println("After Clicking on Show button TextBox Displayed ?:"+b9);
 		
 		TakesScreenshot tk9=(TakesScreenshot)km;
 		
 		File src9=tk9.getScreenshotAs(OutputType.FILE);
-				
+		
 		try {
-			FileUtils.copyFile(src9, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9AFTERCLICKINGONSHOWBUTTON.png"));
+			FileUtils.copyFile(src9, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9AfterClickingSHOWButton.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -321,30 +296,30 @@ public class Day16_9 {
 	@Test
 	public void tc9() throws InterruptedException
 	{
-		JavascriptExecutor js10=(JavascriptExecutor)km;
+		JavascriptExecutor js9=(JavascriptExecutor)km;
 		
-		js10.executeScript("window.scrollBy(0,1400)");
-		
-		Thread.sleep(2000);
-		
-		WebElement frame=km.findElement(By.xpath("//iframe[contains(@id,'courses-iframe')]"));
-		
-		km.switchTo().frame(frame);
+		js9.executeScript("window.scrollBy(0,1400)");
 		
 		Thread.sleep(2000);
 		
-	    JavascriptExecutor js11=(JavascriptExecutor)km;
+		WebElement frm=km.findElement(By.xpath("//iframe[contains(@id,'courses-iframe')]"));
 		
-		js11.executeScript("window.scrollBy(0,1400)");
+		km.switchTo().frame(frm);
 		
 		Thread.sleep(2000);
 		
-		TakesScreenshot tk10=(TakesScreenshot)km;
+        JavascriptExecutor js10=(JavascriptExecutor)km;
 		
-		File src10=tk10.getScreenshotAs(OutputType.FILE);
+		js10.executeScript("window.scrollBy(0,2000)");
+		
+		Thread.sleep(2000);
+		
+		TakesScreenshot tk9=(TakesScreenshot)km;
+		
+		File src9=tk9.getScreenshotAs(OutputType.FILE);
 		
 		try {
-			FileUtils.copyFile(src10, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9FRAME.png"));
+			FileUtils.copyFile(src9, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9FRAME.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -352,47 +327,37 @@ public class Day16_9 {
 	}
 	
 	@Test
-	public void tc10() throws InterruptedException
+	public void tc10() throws InterruptedException, IOException
 	{
-        JavascriptExecutor js11=(JavascriptExecutor)km;
+		JavascriptExecutor js11=(JavascriptExecutor)km;
 		
-		js11.executeScript("window.scrollBy(0,1100)");
+		js11.executeScript("window.scrollBy(0,1200)");
 		
 		Thread.sleep(2000);
 		
 		WebElement mouseHover=km.findElement(By.xpath("//button[contains(@id,'mousehover')]"));
 		
-		WebElement reload=km.findElement(By.xpath("//a[contains(text(),'Reload')]"));
+		WebElement top=km.findElement(By.xpath("//a[contains(text(),'Top')]"));
 		
 		Actions aaa=new Actions(km);
 		
 		aaa.moveToElement(mouseHover).build().perform();
 		
-		aaa.click(reload).build().perform();
+		aaa.click(top).build().perform();
 		
 		Thread.sleep(2000);
 		
 		TakesScreenshot tk10=(TakesScreenshot)km;
 		
-		File src10=tk10.getScreenshotAs(OutputType.FILE);
+		File src10= tk10.getScreenshotAs(OutputType.FILE);
 		
-		try {
-			FileUtils.copyFile(src10, new File("C://Users//Yogesh//git//NewPractice//Screenshot//16_9ACTIONS.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		FileUtils.copyFile(src10, new File("C://Users//Yogesh//git//NewPractice//Screenshot//18_9ACTIONS.png"));
 	}
 	
 	@AfterMethod
 	public void tearDown()
 	{
 		km.quit();
-		
-		test.pass("Closed the Browser");
-		
-		test.info("Test Completed");
-		
-		extent.flush();
 	}
+	
 }
