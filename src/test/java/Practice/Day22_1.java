0a41a30d7d38152c7aaa42f14ac5119964cbd15d
Yogesh <Yogesh@YOGESH>
2026-02-16 14:33:29 +0530

    Selenium Code Commit

--- src/test/java/Practice/Day22_1.java
+++ src/test/java/Practice/Day22_1.java
@@ -19,9 +19,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Day22_1 {
