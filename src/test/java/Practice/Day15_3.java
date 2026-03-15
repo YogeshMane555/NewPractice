e755975f481de87e632e3a5b089f91177ab99aa3
Yogesh <Yogesh@YOGESH>
2026-03-15 21:33:03 +0530

    Selenium Commit 15_6

---
+++ src/test/java/Practice/Day15_3.java
@@ -0,0 +1,282 @@
+package Practice;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Day15_3 {
+
+	public WebDriver km;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Yogesh\\git\\NewPractice\\NewChromeDriver\\chromedriver.exe");
+		
+		ChromeOptions co=new ChromeOptions();
+		
+		co.addArguments("start-maximized");
+		
+		co.addArguments("--incognito");
+		
+		co.addArguments("delete-all-cookies");
+		
+		km=new ChromeDriver(co);
+		
+		km.get("https://rahulshettyacademy.com/AutomationPractice/");
+			
+	}
+	
+	public String screenShot(String fileName)
+	{
+		TakesScreenshot tk=(TakesScreenshot)km;
+		
+		File src=tk.getScreenshotAs(OutputType.FILE);
+		
+		try {
+			FileUtils.copyFile(src, new File("C:\\Users\\Yogesh\\git\\NewPractice\\Screenshot//"+fileName+".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return fileName;
+	}
+	
+	@Test
+	public void tc1()
+	{
+		List<WebElement> tg=km.findElements(By.tagName("a"));
+		
+		int noTg=tg.size();
+		
+		System.out.println("Total Number of Tags :"+noTg);
+		
+		for(WebElement tgT:tg)
+		{
+			String tagText=tgT.getText();
+			
+			System.out.println("Tag are :"+tagText);
+		}
+	}
+	
+	@Test
+	public void tc2()
+	{
+		WebElement r1=km.findElement(By.xpath("//input[contains(@value,'radio1')]"));
+		
+		r1.click();
+		
+		boolean b=r1.isSelected();
+		
+		Assert.assertTrue(b);
+		
+		screenShot("15_3RADIO");
+	}
+	
+	@Test
+	public void tc3() throws InterruptedException
+	{
+		String expected="Russian Federation";
+		
+		WebElement auto=km.findElement(By.xpath("//input[contains(@id,'autocomplete')]"));
+		
+		auto.sendKeys("Fe");
+		
+		Thread.sleep(2000);
+		
+		List<WebElement> autoT=km.findElements(By.xpath("//li[contains(@class,'ui-menu-item')]"));
+		
+		int noauto=autoT.size();
+		
+		System.out.println("Total Number of Autosuggestions are :"+noauto);
+		
+		for(WebElement autoTxt:autoT)
+		{
+			String txt=autoTxt.getText();
+			
+			System.out.println("Autosuggestion are :"+txt);
+			
+			if(txt.contentEquals("Russian Federation"))
+			{
+				autoTxt.click();
+				
+				String actual=auto.getAttribute("value");
+				
+				System.out.println("Selected from Autosuggestion :"+actual);
+				
+				Assert.assertEquals(actual, expected);
+			
+				screenShot("15_3Autosuggestion");
+			}
+		}
+	}
+	
+	@Test
+	public void tc4()
+	{
+		String expected="option1";
+		
+		WebElement drpdwn=km.findElement(By.xpath("//select[contains(@id,'dropdown-class-example')]"));
+		
+		Select s=new Select(drpdwn);
+		
+		s.selectByVisibleText("Option1");
+		
+		String actual=drpdwn.getAttribute("value");
+		
+		Assert.assertEquals(actual, expected);
+		
+		screenShot("15_3Dropdown");
+	}
+	
+	@Test
+	public void tc5()
+	{
+		WebElement c1=km.findElement(By.xpath("//input[contains(@id,'checkBoxOption1')]"));
+		
+		c1.click();
+		
+		boolean b=c1.isSelected();
+		
+		Assert.assertTrue(b);
+		
+		screenShot("15_3Checkbox");
+	}
+	
+	@Test
+	public void tc6() throws InterruptedException
+	{
+		String expected="https://www.dot-consulting.org/articles/foundations-of-modern-higher-education.html?psystem=PW&domain=www.qaclickacademy.com&oref=https%3A%2F%2Frahulshettyacademy.com%2F";
+		
+		km.findElement(By.xpath("//a[contains(@id,'opentab')]")).click();
+		
+		Set<String> s= km.getWindowHandles();
+		
+		Iterator<String> ss=s.iterator();
+		
+		String parent=ss.next();
+		
+		String child=ss.next();
+		
+		km.switchTo().window(child);
+		
+		Thread.sleep(2000);
+		
+		String actual=km.getCurrentUrl();
+		
+		Assert.assertEquals(actual, expected);
+	
+		screenShot("15_3NewTab");
+	}
+	
+	@Test
+	public void tc7() throws InterruptedException
+	{
+		WebElement alTxt=km.findElement(By.xpath("//input[contains(@id,'name')]"));
+		
+		alTxt.sendKeys("Yogesh");
+		
+		km.findElement(By.xpath("//input[contains(@id,'alertbtn')]")).click();
+		
+		Thread.sleep(2000);
+		
+		km.switchTo().alert().accept();
+		
+		alTxt.sendKeys("Mane");
+		
+		km.findElement(By.xpath("//input[contains(@id,'confirmbtn')]")).click();
+		
+		Thread.sleep(2000);
+		
+		km.switchTo().alert().dismiss();
+	}
+	
+	@Test
+	public void tc8()
+	{
+		WebElement eleDisp=km.findElement(By.xpath("//input[contains(@id,'displayed-text')]"));
+		
+		eleDisp.sendKeys("Maa Durga");
+		
+		km.findElement(By.xpath("//input[contains(@id,'hide-textbox')]")).click();
+		
+		boolean b=eleDisp.isDisplayed();
+	
+		Assert.assertFalse(b);
+		
+		screenShot("15_3Hide");
+		
+		km.findElement(By.xpath("//input[contains(@id,'show-textbox')]")).click();
+		
+		boolean b2=eleDisp.isDisplayed();
+		
+		Assert.assertTrue(b2);
+		
+		screenShot("15_3Show");
+	}
+	
+	@Test
+	public void tc9() throws InterruptedException
+	{
+		JavascriptExecutor js=(JavascriptExecutor)km;
+		
+		js.executeScript("window.scrollBy(0,1300)");
+		
+		Thread.sleep(2000);
+		
+		WebElement mouseHover=km.findElement(By.xpath("//button[contains(@id,'mousehover')]"));
+		
+		WebElement top=km.findElement(By.xpath("//a[contains(text(),'Top')]"));
+		
+		Actions a=new Actions(km);
+		
+		a.moveToElement(mouseHover).moveToElement(top).click(top).build().perform();
+		
+		screenShot("15_3Actions");
+	}
+	
+	@Test
+	public void tc10() throws InterruptedException
+	{
+        JavascriptExecutor js=(JavascriptExecutor)km;
+		
+		js.executeScript("window.scrollBy(0,1500)");
+		
+		Thread.sleep(2000);
+		
+		WebElement frame=km.findElement(By.xpath("//iframe[contains(@id,'courses-iframe')]"));
+		
+		km.switchTo().frame(frame);
+		
+        JavascriptExecutor js2=(JavascriptExecutor)km;
+		
+		js2.executeScript("window.scrollBy(0,1500)");
+		
+		Thread.sleep(2000);
+		
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		km.quit();
+	}
+}
